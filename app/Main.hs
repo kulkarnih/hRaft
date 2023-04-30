c6a856c0450a9d4b7c0e06e93d540d1e0b3459f7
Hariprasad Kulkarni <kulkarni.hariprasad@gmail.com>
2023-04-30 11:43:45 +0530

    create node

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main (main) where
 
-import Lib
+import Network.Socket
+import Network.Transport (Transport)
+import Network.Transport.TCP
+import System.Environment (getArgs)
+import Control.Exception (IOException)
+import Control.Distributed.Process.Node
+
+createNode :: HostName -> ServiceName -> IO (Either IOException Transport)
+createNode hostname port = createTransport (defaultTCPAddr hostname port) defaultTCPParameters
 
 main :: IO ()
-main = helloWorld
+main = do
+  args <- getArgs
+  case args of
+    (port : []) -> do
+      transport <- createNode "localhost" port
+      case transport of
+        Right transport -> do
+          putStrLn "Press newline to exit."
+          getLine >> putStrLn "Exiting."
+        Left ioException -> do
+          putStrLn "Could not start the node."
+          print ioException
+    _          -> do
+      putStrLn "Invalid input format. Takes in only a port."
